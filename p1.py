7a421f470d022b7aeae694a8ab5de7b8206adbbb
Abhi-3361 <abhijitdare2005@gmail.com>
2025-01-22 17:44:00 +0530

    Add files via upload

--- p1.py
+++ p1.py
@@ -1,35 +1,11 @@
-# Function to calculate the mean of marks
-def calculate_mean(marks_dict):
-    total_marks = sum(marks_dict.values())  # Sum of all the marks
-    num_students = len(marks_dict)  # Number of students
-    mean = total_marks / num_students  # Calculate mean
-    return mean
-
-# Accept student names and marks from the user
-student_marks = {}
-
-# Ask the user for how many students' data they want to enter
-num_students = int(input("How many students' marks do you want to enter? "))
-
-# Loop to accept input for student names and marks
-for _ in range(num_students):
-    student_name = input("Enter student name: ")
-    
-    while True:
-        try:
-            marks = float(input(f"Enter marks for {student_name}: "))
-            if marks < 0:
-                print("Marks cannot be negative. Please enter a valid mark.")
-            else:
-                student_marks[student_name] = marks
-                break
-        except ValueError:
-            print("Invalid input! Please enter a numeric value for marks.")
-
-# Calculate and display the mean
-if student_marks:
-    mean_marks = calculate_mean(student_marks)
-    print(f"The mean of the marks is: {mean_marks}")
+#defining variable which may cause runtime error in try block
+try:
+    numerator=float(input("enter numerator:"))
+    denominator=float(input("enter denominator:"))
+    result = numerator / denominator
+#exception for 0 at denominator
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")#message will print if error occure
+#if any error does not occure output will be given 
 else:
-    print("No student data to calculate mean.")
-
+    print("result:",result)
--- p2.py
+++ p2.py
@@ -1,11 +1,9 @@
-# Sample Dictionaries
-dic1 = {1: 10, 2: 20}
-dic2 = {3: 30, 4: 40}
-dic3 = {5: 50, 6: 60}
-
-# Method 1: Using the update() method
-result_dict = dic1.copy()  # Create a copy of dic1 to avoid modifying the original
-result_dict.update(dic2)  # Add dic2 to result_dict
-result_dict.update(dic3)  # Add dic3 to result_dict
-
-print("Concatenated Dictionary:", result_dict)
+#defining variable which may cause runtime error in try block
+try:
+    user_input=int(input("enter integer:"))
+#exception for invalid input error
+except ValueError:
+    print("please enter a valid integer")
+#output for valid input
+else:
+    print("you entered:",user_input)
